5db44173b0204a45ab1da5925f8d16ae51d9c970
mutahirshah11 <mutahirhps@gmail.com>
2024-10-11 00:25:33 +0500

    changes

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -38,7 +38,7 @@ const Hero = () => {
 
          <div className= ' absolute text-[#F4F4F4] max-w-[350px] h-[100px] ml-44 mt-48 md:ml-[270px] lg:mt-[260px] xl:mt-[320px] animate-bounceIn transition duration-1000'>
           
-        <div className='inline-flex'><span className='text-white text-[20px] font-bold gap-1 md:text-[25px]  lg:text-[33px]  lg:w-[500px] xl:text-[35px]  '>NEW COLLECTION <p className=' text-[#E8CF36] '>IN DIAMONDS&quot</p></span></div>
+        <div className='inline-flex'><span className='text-white text-[20px] font-bold gap-1 md:text-[25px]  lg:text-[33px]  lg:w-[500px] xl:text-[35px]  '>NEW COLLECTION <p className=' text-[#E8CF36] '>IN DIAMONDS</p></span></div>
         
         <p className='text-[10px]  text-[#E7E7E7] font-sans mt-1 md:text-[12px] md:w-[420px] lg:text-[15px] lg:w-[530px] xl:text-[16px] xl:w-[650px] '>Step into the world of timeless elegance and unparalleled brilliance with our brand-new diamond collection. Each piece is meticulously crafted to perfection, combining the finest quality diamonds with stunning designs </p>
         <button className='text-[#E8CF36] border-[#E8CF36] border-2 rounded-md text-[10px] md:text-[13px] px-3 py-1 md:px-6 md:py-2.5 mt-2 lg:text-[15px] lg:mt-5 xl:text-[16px] xl:mt-6 hover:bg-yellow-400/90 hover:text-black hover:font-semibold'>FIND OUT MORE</button>
